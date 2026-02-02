63868fd7b897a5eaf7c5921cfdbd2402c331a9d6
ppguz <ppguzmanmayen@gmail.com>
2026-02-02 10:45:34 -0600

    Feat: implemented function for rot13 cipher

---
+++ rot13.py
@@ -0,0 +1,7 @@
+from caesar import cipher_caesar, decipher_caesar
+
+def cipher_rot13(message):
+    return cipher_caesar(message, 13)
+
+def decihper_rot13(message):
+    return decipher_caesar(message, 13)
